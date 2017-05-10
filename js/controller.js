3afe9e7c8c11f87a40f5a472c89b587c9d435b96
HENRY HOBHOUSE <henryhobhouse@gmail.com>
2017-05-10 23:42:13 +0100

    Background changes colour dependant on energy usage. Add basic CSS styling

---
+++ js/controller.js
@@ -0,0 +1,37 @@
+
+$("#increasetemp").click(function() {
+  thermostat.uptemp();
+  update_temp()
+});
+
+$("#decreasetemp").click(function() {
+  thermostat.downtemp();
+  update_temp()
+});
+
+$("#temp_reset").click(function() {
+  thermostat.temp_reset();
+  update_temp()
+});
+
+$("#power_saver").change(function() {
+  thermostat.saver_switch();
+  $("#power_status").text( "Power Saver Status: " + thermostat.powersaver );
+  update_max()
+  update_temp()
+});
+
+update_temp()
+update_max()
+$("#max").text( "MIN temp: " + thermostat.MIN );
+$("#power_status").text( "Power Saver Status: " + thermostat.powersaver );
+
+
+function update_temp() {
+  $("#current").text( "current_temperature: " + thermostat.current_temperature );
+  $("#body").attr('class', thermostat.energy_usage );
+};
+
+function update_max() {
+  $("#min").text( "MAX temp: " + thermostat.max );
+};
